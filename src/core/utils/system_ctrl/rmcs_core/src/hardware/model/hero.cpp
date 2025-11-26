920b1980ab2d2cadc54f57c3225e3aee5f6df545
MIDE酱 <mide233@outlook.com>
2025-11-26 14:21:41 +0000

    finish hero model

--- src/core/utils/system_ctrl/rmcs_core/src/hardware/model/hero.cpp
+++ src/core/utils/system_ctrl/rmcs_core/src/hardware/model/hero.cpp
@@ -11,13 +11,10 @@
 #include <serial/serial.h>
 #include <std_msgs/msg/int32.hpp>
 
-#include "hardware/device/benewake.hpp"
 #include "hardware/device/bmi088.hpp"
 #include "hardware/device/dji_motor.hpp"
+#include "hardware/device/dm_motor.hpp"
 #include "hardware/device/dr16.hpp"
-#include "hardware/device/gy614.hpp"
-#include "hardware/device/hipnuc.hpp"
-#include "hardware/device/lk_motor.hpp"
 #include "hardware/device/supercap.hpp"
 
 namespace rmcs_core::hardware {
@@ -65,10 +62,10 @@ public:
 private:
     void gimbal_calibrate_subscription_callback(std_msgs::msg::Int32::UniquePtr) {
         RCLCPP_INFO(
-            get_logger(), "[gimbal calibration] New yaw offset: %ld",
-            bottom_board_->gimbal_yaw_motor_.calibrate_zero_point());
+            get_logger(), "[gimbal calibration] New yaw offset: %d",
+            top_board_->gimbal_yaw_motor_.calibrate_zero_point());
         RCLCPP_INFO(
-            get_logger(), "[gimbal calibration] New pitch offset: %ld",
+            get_logger(), "[gimbal calibration] New pitch offset: %d",
             top_board_->gimbal_pitch_motor_.calibrate_zero_point());
     }
 
@@ -89,34 +86,27 @@ private:
         explicit TopBoard(Hero& hero, HeroCommand& hero_command, int usb_pid = -1)
             : librmcs::client::CBoard(usb_pid)
             , tf_(hero.tf_)
-            , imu_(1000, 0.2, 0.0)
-            , gy614_(hero, "/friction_wheels/temperature")
-            , benewake_(hero, "/gimbal/auto_aim/laser_distance")
+            , imu_(10.0f, 0.001f, 1000000.0f)
+            , dr16_(hero)
             , gimbal_pitch_motor_(
                   hero, hero_command, "/gimbal/pitch",
-                  device::LkMotor::Config{device::LkMotor::Type::MG5010E_I10}
-                      .set_encoder_zero_point(
-                          static_cast<int>(hero.get_parameter("pitch_motor_zero_point").as_int())))
+                  device::DmMotor::Config{device::DmMotor::Type::J4310}.set_encoder_zero_point(
+                      static_cast<int>(hero.get_parameter("pitch_motor_zero_point").as_int())))
+            , gimbal_yaw_motor_(
+                  hero, hero_command, "/gimbal/yaw",
+                  device::DjiMotor::Config{device::DjiMotor::Type::GM6020}.set_encoder_zero_point(
+                      static_cast<int>(hero.get_parameter("yaw_motor_zero_point").as_int())))
+            , gimbal_bullet_feeder_(
+                  hero, hero_command, "/gimbal/bullet_feeder",
+                  device::DmMotor::Config{device::DmMotor::Type::J4310}.enable_multi_turn_angle())
             // TODO: Bad CAN ID sequence, needs to be adjusted.
             , gimbal_friction_wheels_(
-                  {hero, hero_command, "/gimbal/second_left_friction",
+                  {hero, hero_command, "/gimbal/first_friction",
                    device::DjiMotor::Config{device::DjiMotor::Type::M3508}.set_reduction_ratio(1.)},
-                  {hero, hero_command, "/gimbal/second_right_friction",
-                   device::DjiMotor::Config{device::DjiMotor::Type::M3508}
-                       .set_reduction_ratio(1.)
-                       .set_reversed()},
-                  {hero, hero_command, "/gimbal/first_left_friction",
+                  {hero, hero_command, "/gimbal/second_friction",
                    device::DjiMotor::Config{device::DjiMotor::Type::M3508}.set_reduction_ratio(1.)},
-                  {hero, hero_command, "/gimbal/first_right_friction",
-                   device::DjiMotor::Config{device::DjiMotor::Type::M3508}
-                       .set_reduction_ratio(1.)
-                       .set_reversed()})
-            , gimbal_scope_motor_(
-                  hero, hero_command, "/gimbal/scope",
-                  device::DjiMotor::Config{device::DjiMotor::Type::M2006})
-            , gimbal_player_viewer_motor_(
-                  hero, hero_command, "/gimbal/player_viewer",
-                  device::LkMotor::Config{device::LkMotor::Type::MG4005E_I10})
+                  {hero, hero_command, "/gimbal/third_friction",
+                   device::DjiMotor::Config{device::DjiMotor::Type::M3508}.set_reduction_ratio(1.)})
             , transmit_buffer_(*this, 32)
             , event_thread_([this]() { handle_events(); }) {
 
@@ -136,68 +126,51 @@ private:
             hero.register_output("/gimbal/yaw/velocity_imu", gimbal_yaw_velocity_imu_);
             hero.register_output("/gimbal/pitch/velocity_imu", gimbal_pitch_velocity_imu_);
 
-            external_imu_thread_ = std::jthread([this, &hero](const std::stop_token& stop_token) {
-                external_imu_thread_main(
-                    stop_token, hero.get_parameter("external_imu_port").as_string(),
-                    hero.get_logger());
-            });
+            hero.register_output("/debug/pitch/raw_angle", debug_pitch_raw_angle_);
         }
 
         ~TopBoard() final {
             stop_handling_events();
             event_thread_.join();
-            external_imu_thread_.request_stop();
         }
 
         void update() {
             imu_.update_status();
             Eigen::Quaterniond gimbal_imu_pose{imu_.q0(), imu_.q1(), imu_.q2(), imu_.q3()};
 
-            if (external_imu_available_.load(std::memory_order::relaxed)) {
-                external_imu_.update_status();
-                gimbal_imu_pose.slerp(0.001, external_imu_.quaternion());
-                imu_.q0() = gimbal_imu_pose.w();
-                imu_.q1() = gimbal_imu_pose.x();
-                imu_.q2() = gimbal_imu_pose.y();
-                imu_.q3() = gimbal_imu_pose.z();
-            }
-
             tf_->set_transform<rmcs_description::PitchLink, rmcs_description::OdomImu>(
                 gimbal_imu_pose.conjugate());
 
-            gy614_.update_status();
-            benewake_.update_status();
+            dr16_.update_status();
 
             *gimbal_yaw_velocity_imu_ = imu_.gz();
             *gimbal_pitch_velocity_imu_ = imu_.gy();
 
+            *debug_pitch_raw_angle_ = gimbal_pitch_motor_.last_raw_angle();
+
             gimbal_pitch_motor_.update_status();
             tf_->set_state<rmcs_description::YawLink, rmcs_description::PitchLink>(
                 gimbal_pitch_motor_.angle());
+            gimbal_yaw_motor_.update_status();
+            tf_->set_state<rmcs_description::GimbalCenterLink, rmcs_description::YawLink>(
+                gimbal_yaw_motor_.angle());
+
+            gimbal_bullet_feeder_.update_status();
 
             for (auto& motor : gimbal_friction_wheels_)
                 motor.update_status();
-
-            gimbal_player_viewer_motor_.update_status();
         }
 
         void command_update() {
             uint16_t batch_commands[4];
-            for (int i = 0; i < 4; i++)
+
+            for (int i = 0; i < 3; i++)
                 batch_commands[i] = gimbal_friction_wheels_[i].generate_command();
+            batch_commands[3] = gimbal_yaw_motor_.generate_command();
             transmit_buffer_.add_can1_transmission(0x200, std::bit_cast<uint64_t>(batch_commands));
 
-            transmit_buffer_.add_can2_transmission(0x142, gimbal_pitch_motor_.generate_command());
-
-            batch_commands[0] = gimbal_scope_motor_.generate_command();
-            batch_commands[1] = 0;
-            batch_commands[2] = 0;
-            batch_commands[3] = 0;
-            transmit_buffer_.add_can1_transmission(0x1ff, std::bit_cast<uint64_t>(batch_commands));
-
-            transmit_buffer_.add_can2_transmission(
-                0x141, gimbal_player_viewer_motor_.generate_angle_command(
-                           gimbal_player_viewer_motor_.control_angle()));
+            transmit_buffer_.add_can2_transmission(0x209, gimbal_pitch_motor_.generate_command());
+            transmit_buffer_.add_can2_transmission(0x204, gimbal_bullet_feeder_.generate_command());
 
             transmit_buffer_.trigger_transmission();
         }
@@ -216,7 +189,7 @@ private:
             } else if (can_id == 0x203) {
                 gimbal_friction_wheels_[2].store_status(can_data);
             } else if (can_id == 0x204) {
-                gimbal_friction_wheels_[3].store_status(can_data);
+                gimbal_yaw_motor_.store_status(can_data);
             }
         }
 
@@ -226,19 +199,15 @@ private:
             if (is_extended_can_id || is_remote_transmission || can_data_length < 8) [[unlikely]]
                 return;
 
-            if (can_id == 0x142) {
+            if (can_id == 0x219) {
                 gimbal_pitch_motor_.store_status(can_data);
-            } else if (can_id == 0x141) {
-                gimbal_player_viewer_motor_.store_status(can_data);
+            } else if (can_id == 0x214) {
+                gimbal_bullet_feeder_.store_status(can_data);
             }
         }
 
-        void uart1_receive_callback(const std::byte* data, uint8_t length) override {
-            benewake_.store_status(data, length);
-        }
-
-        void uart2_receive_callback(const std::byte* data, uint8_t length) override {
-            gy614_.store_status(data, length);
+        void dbus_receive_callback(const std::byte* uart_data, uint8_t uart_data_length) override {
+            dr16_.store_status(uart_data, uart_data_length);
         }
 
         void accelerometer_receive_callback(int16_t x, int16_t y, int16_t z) override {
@@ -249,53 +218,23 @@ private:
             imu_.store_gyroscope_status(x, y, z);
         }
 
-        void external_imu_thread_main(
-            const std::stop_token& stop_token, const std::string& port_name,
-            const rclcpp::Logger& logger) {
-            try {
-                serial::Serial serial{port_name, 115200, serial::Timeout::simpleTimeout(10)};
-                rmcs_utility::FpsCounter fps_counter;
-
-                while (!stop_token.stop_requested()) {
-                    if (external_imu_.store_status<uint8_t>(serial) && fps_counter.count()) {
-                        bool available = fps_counter.fps() > 350.0;
-                        if (!available)
-                            RCLCPP_WARN(logger, "External IMU low FPS: %.2f", fps_counter.fps());
-                        else if (!external_imu_available_.load(std::memory_order::relaxed))
-                            RCLCPP_INFO(
-                                logger, "External IMU now available with FPS: %.2f",
-                                fps_counter.fps());
-                        external_imu_available_.store(available, std::memory_order::relaxed);
-                    }
-                }
-            } catch (const std::exception& e) {
-                external_imu_available_.store(false, std::memory_order::relaxed);
-                RCLCPP_ERROR(logger, "Exception in external IMU thread: %s", e.what());
-            }
-        }
-
         OutputInterface<rmcs_description::Tf>& tf_;
 
         device::Bmi088 imu_;
-        device::Gy614 gy614_;
-        device::Benewake benewake_;
+        device::Dr16 dr16_;
 
         OutputInterface<double> gimbal_yaw_velocity_imu_;
         OutputInterface<double> gimbal_pitch_velocity_imu_;
+        OutputInterface<double> debug_pitch_raw_angle_;
 
-        device::LkMotor gimbal_pitch_motor_;
+        device::DmMotor gimbal_pitch_motor_;
+        device::DjiMotor gimbal_yaw_motor_;
 
-        device::DjiMotor gimbal_friction_wheels_[4];
-
-        device::DjiMotor gimbal_scope_motor_;
-        device::LkMotor gimbal_player_viewer_motor_;
+        device::DmMotor gimbal_bullet_feeder_;
+        device::DjiMotor gimbal_friction_wheels_[3];
 
         librmcs::client::CBoard::TransmitBuffer transmit_buffer_;
         std::thread event_thread_;
-
-        rmcs_core::hardware::device::Hipnuc external_imu_;
-        std::atomic<bool> external_imu_available_ = false;
-        std::jthread external_imu_thread_;
     };
 
     class BottomBoard final : private librmcs::client::CBoard {
@@ -303,9 +242,8 @@ private:
         friend class Hero;
         explicit BottomBoard(Hero& hero, HeroCommand& hero_command, int usb_pid = -1)
             : librmcs::client::CBoard(usb_pid)
-            , imu_(1000, 0.2, 0.0)
+            , imu_(10.0f, 0.001f, 1000000.0f)
             , tf_(hero.tf_)
-            , dr16_(hero)
             , chassis_wheel_motors_(
                   {hero, hero_command, "/chassis/left_front_wheel",
                    device::DjiMotor::Config{device::DjiMotor::Type::M3508}},
@@ -316,18 +254,21 @@ private:
                   {hero, hero_command, "/chassis/right_front_wheel",
                    device::DjiMotor::Config{device::DjiMotor::Type::M3508}})
             , supercap_(hero, hero_command)
-            , gimbal_yaw_motor_(
-                  hero, hero_command, "/gimbal/yaw",
-                  device::LkMotor::Config{device::LkMotor::Type::MG5010E_I10}
-                      .set_encoder_zero_point(
-                          static_cast<int>(hero.get_parameter("yaw_motor_zero_point").as_int())))
-            , gimbal_bullet_feeder_(
-                  hero, hero_command, "/gimbal/bullet_feeder",
-                  device::LkMotor::Config{device::LkMotor::Type::MG5010E_I10}
-                      .set_reversed()
-                      .enable_multi_turn_angle())
             , transmit_buffer_(*this, 32)
             , event_thread_([this]() { handle_events(); }) {
+
+            imu_.set_coordinate_mapping([](double x, double y, double z) {
+                // Get the mapping with the following code.
+                // The rotation angle must be an exact multiple of 90 degrees, otherwise use a
+                // matrix.
+
+                // Eigen::AngleAxisd pitch_link_to_imu_link{
+                //     std::numbers::pi, Eigen::Vector3d::UnitZ()};
+                // Eigen::Vector3d mapping = pitch_link_to_imu_link * Eigen::Vector3d{1, 2, 3};
+                // std::cout << mapping << std::endl;
+
+                return std::make_tuple(x, y, z);
+            });
 
             hero.register_output("/referee/serial", referee_serial_);
             referee_serial_->read = [this](std::byte* buffer, size_t size) {
@@ -339,6 +280,7 @@ private:
                 return size;
             };
 
+
             hero.register_output("/chassis/yaw/velocity_imu", chassis_yaw_velocity_imu_, 0);
         }
 
@@ -349,7 +291,6 @@ private:
 
         void update() {
             imu_.update_status();
-            dr16_.update_status();
 
             *chassis_yaw_velocity_imu_ = imu_.gz();
 
@@ -357,12 +298,6 @@ private:
                 motor.update_status();
 
             supercap_.update_status();
-
-            gimbal_yaw_motor_.update_status();
-            tf_->set_state<rmcs_description::GimbalCenterLink, rmcs_description::YawLink>(
-                gimbal_yaw_motor_.angle());
-
-            gimbal_bullet_feeder_.update_status();
         }
 
         void command_update() {
@@ -371,18 +306,6 @@ private:
             for (int i = 0; i < 4; i++)
                 batch_commands[i] = chassis_wheel_motors_[i].generate_command();
             transmit_buffer_.add_can1_transmission(0x200, std::bit_cast<uint64_t>(batch_commands));
-
-            transmit_buffer_.add_can1_transmission(
-                0x141, gimbal_bullet_feeder_.generate_torque_command(
-                           gimbal_bullet_feeder_.control_torque()));
-
-            transmit_buffer_.add_can2_transmission(0x141, gimbal_yaw_motor_.generate_command());
-
-            batch_commands[0] = 0;
-            batch_commands[1] = 0;
-            batch_commands[2] = 0;
-            batch_commands[3] = supercap_.generate_command();
-            transmit_buffer_.add_can2_transmission(0x1FE, std::bit_cast<uint64_t>(batch_commands));
 
             transmit_buffer_.trigger_transmission();
         }
@@ -402,8 +325,6 @@ private:
                 chassis_wheel_motors_[2].store_status(can_data);
             } else if (can_id == 0x204) {
                 chassis_wheel_motors_[3].store_status(can_data);
-            } else if (can_id == 0x141) {
-                gimbal_bullet_feeder_.store_status(can_data);
             }
         }
 
@@ -413,20 +334,9 @@ private:
             if (is_extended_can_id || is_remote_transmission || can_data_length < 8) [[unlikely]]
                 return;
 
-            if (can_id == 0x141) {
-                gimbal_yaw_motor_.store_status(can_data);
-            } else if (can_id == 0x300) {
+            if (can_id == 0x300) {
                 supercap_.store_status(can_data);
             }
-        }
-
-        void uart1_receive_callback(const std::byte* uart_data, uint8_t uart_data_length) override {
-            referee_ring_buffer_receive_.emplace_back_multi(
-                [&uart_data](std::byte* storage) { *storage = *uart_data++; }, uart_data_length);
-        }
-
-        void dbus_receive_callback(const std::byte* uart_data, uint8_t uart_data_length) override {
-            dr16_.store_status(uart_data, uart_data_length);
         }
 
         void accelerometer_receive_callback(int16_t x, int16_t y, int16_t z) override {
@@ -442,14 +352,8 @@ private:
 
         OutputInterface<double> chassis_yaw_velocity_imu_;
 
-        device::Dr16 dr16_;
-
         device::DjiMotor chassis_wheel_motors_[4];
         device::Supercap supercap_;
-
-        device::LkMotor gimbal_yaw_motor_;
-
-        device::LkMotor gimbal_bullet_feeder_;
 
         librmcs::utility::RingBuffer<std::byte> referee_ring_buffer_receive_{256};
         OutputInterface<rmcs_msgs::SerialInterface> referee_serial_;
