4fd74eacaa0d11fba1c6ead4f677a73be3c2bfab
MIDE酱 <mide233@outlook.com>
2025-12-01 11:26:56 +0000

    完成云台求解器优化与hero车体pid调参

--- src/core/utils/system_ctrl/rmcs_core/src/hardware/model/hero.cpp
+++ src/core/utils/system_ctrl/rmcs_core/src/hardware/model/hero.cpp
@@ -269,8 +269,8 @@ private:
         device::DmMotor gimbal_bullet_feeder_;
         device::DjiMotor gimbal_friction_wheels_[3];
 
-        rmcs_core::utility::LowPassFilter<> imu_gy_velocity_filter_{60.0f, 1000.0f};
-        rmcs_core::utility::LowPassFilter<> imu_gz_velocity_filter_{60.0f, 1000.0f};
+        rmcs_core::utility::LowPassFilter<> imu_gy_velocity_filter_{4.0f, 1000.0f};
+        rmcs_core::utility::LowPassFilter<> imu_gz_velocity_filter_{8.0f, 1000.0f};
 
         librmcs::client::CBoard::TransmitBuffer transmit_buffer_;
         std::thread event_thread_;
