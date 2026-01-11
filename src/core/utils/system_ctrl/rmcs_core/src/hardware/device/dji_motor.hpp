cf8f71a76cc0d98aed3154cf39d50c1b86d593ba
mide233 <mide233@outlook.com>
2026-01-11 04:42:41 +0000

    为DJI/DM电机组件添加原始编码器输出便于调零

--- src/core/utils/system_ctrl/rmcs_core/src/hardware/device/dji_motor.hpp
+++ src/core/utils/system_ctrl/rmcs_core/src/hardware/device/dji_motor.hpp
@@ -13,6 +13,7 @@ public:
         const std::string& name_prefix)
         : librmcs::device::DjiMotor() {
         status_component.register_output(name_prefix + "/angle", angle_, 0.0);
+        status_component.register_output(name_prefix + "/raw_angle", raw_angle_, 0.0);
         status_component.register_output(name_prefix + "/velocity", velocity_, 0.0);
         status_component.register_output(name_prefix + "/torque", torque_, 0.0);
         status_component.register_output(name_prefix + "/max_torque", max_torque_, 0.0);
@@ -38,6 +39,7 @@ public:
     void update_status() {
         librmcs::device::DjiMotor::update_status();
         *angle_ = angle();
+        *raw_angle_ = last_raw_angle();
         *velocity_ = velocity();
         *torque_ = torque();
         *velocity_filtered_ = velocity_lpf_.update(velocity());
@@ -56,6 +58,7 @@ public:
 
 private:
     rmcs_executor::Component::OutputInterface<double> angle_;
+    rmcs_executor::Component::OutputInterface<double> raw_angle_;
     rmcs_executor::Component::OutputInterface<double> velocity_;
     rmcs_executor::Component::OutputInterface<double> velocity_filtered_;
     rmcs_executor::Component::OutputInterface<double> torque_;
--- src/core/utils/system_ctrl/rmcs_core/src/hardware/device/dm_motor.hpp
+++ src/core/utils/system_ctrl/rmcs_core/src/hardware/device/dm_motor.hpp
@@ -13,6 +13,7 @@ public:
         const std::string& name_prefix)
         : librmcs::device::DmMotor() {
         status_component.register_output(name_prefix + "/angle", angle_, 0.0);
+        status_component.register_output(name_prefix + "/raw_angle", raw_angle_, 0.0);
         status_component.register_output(name_prefix + "/velocity", velocity_, 0.0);
         status_component.register_output(name_prefix + "/torque", torque_, 0.0);
         status_component.register_output(name_prefix + "/max_torque", max_torque_, 0.0);
@@ -40,6 +41,7 @@ public:
     void update_status() {
         librmcs::device::DmMotor::update_status();
         *angle_ = angle();
+        *raw_angle_ = last_raw_angle();
         *velocity_ = velocity();
         *torque_ = torque();
         *velocity_filtered_ = velocity_lpf_.update(velocity());
@@ -67,6 +69,7 @@ public:
 
 private:
     rmcs_executor::Component::OutputInterface<double> angle_;
+    rmcs_executor::Component::OutputInterface<double> raw_angle_;
     rmcs_executor::Component::OutputInterface<double> velocity_;
     rmcs_executor::Component::OutputInterface<double> velocity_filtered_;
     rmcs_executor::Component::OutputInterface<double> torque_;
