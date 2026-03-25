fbc37e42e99e409ea91b5fc73a91b42871b893b0
MIDE酱 <mide233@outlook.com>
2026-03-25 06:11:12 +0000

    修复野蛮使用while导致的性能异常

--- src/core/utils/system_ctrl/rmcs_core/src/referee/app/ui/dual_infantry.cpp
+++ src/core/utils/system_ctrl/rmcs_core/src/referee/app/ui/dual_infantry.cpp
@@ -12,6 +12,7 @@
 #include <rmcs_msgs/operate_mode.hpp>
 #include <rmcs_msgs/shoot_mode.hpp>
 #include <rmcs_utility/tick_timer.hpp>
+#include <utility/angle_warper.hpp>
 
 #include "referee/app/ui/shape/shape.hpp"
 #include "referee/app/ui/widget/status_bar.hpp"
@@ -143,12 +144,9 @@ private:
             return static_cast<int>(
                 std::round((2 * std::numbers::pi - angle) / std::numbers::pi * 180));
         };
-        auto unify_angle = [](int angle) {
-            while (angle >= 360)
-                angle -= 360;
-            while (angle < 0)
-                angle += 360;
-            return angle;
+        auto unify_angle = [](int angle) constexpr noexcept {
+            angle %= 360;
+            return (angle < 0) ? (angle + 360) : angle;
         };
 
         for (int i = 0; i < 4; ++i) {
